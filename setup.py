b5aa8be1473ca4fab694535e3e5725743c7ae941
Ruby Byrne <rlbyrne@uw.edu>
2023-07-05 14:10:38 -0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="NewCal",
+    author="Ruby Byrne",
+    author_email="rbyrne@caltech.edu",
+    url="https://github.com/rlbyrne/newcal",
+    scripts=["calibration.py", "cost_function_calculations.py"],
+)
