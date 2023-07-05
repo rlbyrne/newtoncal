674c2eedab62cb2e8685e3f759715f062b5a35b2
Ruby Byrne <rlbyrne@uw.edu>
2023-07-05 16:23:07 -0700

    new tests

--- newcal/tests.py
+++ newcal/tests.py
@@ -6,6 +6,7 @@ import os
 import unittest
 
 # Run all tests with pytest tests.py
+# Run one test with pytest tests.py::TestStringMethods::test_name
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -1175,7 +1176,67 @@ class TestStringMethods(unittest.TestCase):
 
         np.testing.assert_allclose(grad_approx, hess_value, rtol=1e-5)
 
-    def test_calibration_single_pol_with_identical_data(self):
+    def test_hess_hermitian(self, verbose=False):
+
+        test_freq_ind = 0
+        test_pol_ind = 0
+        lambda_val = 0.01
+        gain_stddev = 0.1
+
+        model = pyuvdata.UVData()
+        model.read(f"{THIS_DIR}/data/test_model_1freq.uvfits")
+        data = pyuvdata.UVData()
+        data.read(f"{THIS_DIR}/data/test_data_1freq.uvfits")
+
+        (
+            gains_init,
+            Nants,
+            Nbls,
+            Ntimes,
+            Nfreqs,
+            model_visibilities,
+            data_visibilities,
+            visibility_weights,
+            gains_exp_mat_1,
+            gains_exp_mat_2,
+        ) = calibration.uvdata_calibration_setup(data, model)
+
+        np.random.seed(0)
+        gains_init_real = np.random.normal(
+            1.0,
+            gain_stddev,
+            size=(Nants, Nfreqs),
+        )
+        np.random.seed(0)
+        gains_init_imag = 1.0j * np.random.normal(
+            0.0,
+            gain_stddev,
+            size=(Nants, Nfreqs),
+        )
+        gains_init = gains_init_real + gains_init_imag
+        gains_flattened = np.stack(
+            (
+                np.real(gains_init[:, test_freq_ind]),
+                np.imag(gains_init[:, test_freq_ind]),
+            ),
+            axis=0,
+        ).flatten()
+
+        hess = calibration.hessian_single_pol_wrapper(
+            gains_flattened,
+            Nants,
+            Nbls,
+            model_visibilities[:, :, test_freq_ind, test_pol_ind],
+            data_visibilities[:, :, test_freq_ind, test_pol_ind],
+            visibility_weights[:, :, test_freq_ind, test_pol_ind],
+            gains_exp_mat_1,
+            gains_exp_mat_2,
+            lambda_val,
+        )
+
+        np.testing.assert_allclose(hess - np.conj(hess.T), 0.0 + 1j * 0.0)
+
+    def test_calibration_single_pol_identical_data_no_flags(self):
 
         lambda_val = 100.0
 
@@ -1199,6 +1260,74 @@ class TestStringMethods(unittest.TestCase):
             model,
             gain_init_stddev=0.1,
         )
+
+        # Unflag all
+        visibility_weights = np.ones(
+            (
+                Ntimes,
+                Nbls,
+                Nfreqs,
+                4,
+            ),
+            dtype=float,
+        )
+
+        gains_fit = calibration.run_calibration_optimization_per_pol(
+            gains_init,
+            Nants,
+            Nbls,
+            Nfreqs,
+            2,
+            model_visibilities,
+            data_visibilities,
+            visibility_weights,
+            gains_exp_mat_1,
+            gains_exp_mat_2,
+            lambda_val,
+            xtol=1e-8,
+        )
+
+        np.testing.assert_allclose(np.abs(gains_fit), 1.0)
+        np.testing.assert_allclose(np.angle(gains_fit), 0.0, atol=1e-6)
+
+    def test_calibration_single_pol_identical_data_with_flags(self):
+
+        lambda_val = 100.0
+
+        model = pyuvdata.UVData()
+        model.read(f"{THIS_DIR}/data/test_model_1freq.uvfits")
+        data = model.copy()
+
+        (
+            gains_init,
+            Nants,
+            Nbls,
+            Ntimes,
+            Nfreqs,
+            model_visibilities,
+            data_visibilities,
+            visibility_weights,
+            gains_exp_mat_1,
+            gains_exp_mat_2,
+        ) = calibration.uvdata_calibration_setup(
+            data,
+            model,
+            gain_init_stddev=0.1,
+        )
+
+        # Unflag all
+        visibility_weights = np.ones(
+            (
+                Ntimes,
+                Nbls,
+                Nfreqs,
+                4,
+            ),
+            dtype=float,
+        )
+        # Set flags
+        visibility_weights[2, 10, 0, :] = 0.0
+        visibility_weights[1, 20, 0, :] = 0.0
 
         gains_fit = calibration.run_calibration_optimization_per_pol(
             gains_init,
