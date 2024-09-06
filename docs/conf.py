12b1762c80e16d6b2748945950baefef3992e4a5
Ruby Byrne <rlbyrne@uw.edu>
2024-09-05 17:32:42 -0700

    try other sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../newcal/"))
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autoapi"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
